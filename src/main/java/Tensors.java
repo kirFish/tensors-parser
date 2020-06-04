939af9ee35ecdbf947768b0dd842caf112d322ab
kirichripper <kir.fishchuk2014@gmail.com>
2020-06-04 22:11:20 +0300

    the project skeleton

---
+++ src/main/java/Tensors.java
@@ -0,0 +1,64 @@
+import org.tensorics.core.tensor.Tensor;
+
+public class Tensors {
+
+
+    public Tensor<Double> extractSubTensor(){
+        return null;
+    }
+
+
+
+    public Tensor<Double> mathematicalStructures(){
+        return null;
+    }
+
+
+
+    //one argument operation like
+    //mod
+    public Tensor<Double> unaryOperations(){
+        return null;
+    }
+
+
+
+    // as well one argument operation which gives
+    public Tensor<Double> basicStatistics(){
+        return null;
+    }
+
+
+
+
+
+    //
+    public Tensor<Double> binaryOperations(){
+        return null;
+    }
+
+
+
+
+
+    public Tensor<Double> reshapingTensor(){
+        return null;
+    }
+
+
+
+    public Tensor<Double> broadcasting(){
+        return null;
+    }
+
+
+
+    public Tensor<Double> innerProduct(){
+        return null;
+    }
+
+
+
+
+
+}
